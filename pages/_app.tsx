f844a19f3580f0272b6444465f38749f5b74435d
Yuliia Pavliuk <olga96855@gmail.com>
2023-10-02 16:04:11 +0300

    'added test header and footer'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Header } from './components/NavBar/NavBar'
+import { Footer } from './components/FooterFiles/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider>
+			<Header />
+			<Component {...pageProps} />
+			<Footer />
+		</ChakraProvider>
+	)
 }
