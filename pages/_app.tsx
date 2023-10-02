7788f2073abb7d828dd3ee8418bd89e9dd281638
Yuliia Pavliuk <olga96855@gmail.com>
2023-10-02 19:01:57 +0300

    'test 2'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Header } from './components/NavBar/NavBar'
+import { Header } from './components/NavBar/Navbar'
 import { Footer } from './components/FooterFiles/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
