3c8b86197ba3f7b6648394bea1b7ad2d34da5d41
Val Rakitine <vrakitine@gmail.com>
2023-10-04 11:53:16 -0400

    Moved <Sidebar /> to Layout

--- app/Layout.tsx
+++ app/Layout.tsx
@@ -1,7 +1,10 @@
+import Sidebar from '../components/Sidebar'
+
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
 	return (
 		<div>
-			<div>Here is available some layout</div>
+			<div>__Here is available some layout</div>
+			<Sidebar />
 			{children}
 		</div>
 	)
