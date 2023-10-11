e17f194c6e1a4b69cc7970613e8108c26a3c500e
Val Rakitine <vrakitine@gmail.com>
2023-10-11 16:34:00 -0400

    Updated Layout

--- app/Layout.tsx
+++ app/Layout.tsx
@@ -3,7 +3,6 @@ import Sidebar from '../components/Sidebar'
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
 	return (
 		<div>
-			<div>__Here is available some layout</div>
 			<Sidebar />
 			{children}
 		</div>
