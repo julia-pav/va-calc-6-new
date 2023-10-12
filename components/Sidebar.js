af001af2ed9c99074e0d7786debf49ee3a806525
Yuliia Pavliuk <olga96855@gmail.com>
2023-10-12 16:12:19 +0300

    reduced sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -12,57 +12,54 @@ export default function Sidebar() {
 
 	// const toggleCalcBase10 = () => {
 	// 	setCalcBase10Open(!isCalcBase10Open)
-	// }
+	//
 
 	return (
+		// <Flex
+		// 	pos='sticky'
+		// 	left='5'
+		// 	h='95vh'
+		// 	marginTop='2.5vh'
+		// 	boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.05)'
+		// 	borderRadius={navSize == 'small' ? '15px' : '30px'}
+		// 	w={navSize == 'small' ? '75px' : '200px'}
+		// 	flexDir='column'
+		// 	justifyContent='space-between'
+		// >
 		<Flex
-			pos='sticky'
-			left='5'
-			h='95vh'
-			marginTop='2.5vh'
-			boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.05)'
-			borderRadius={navSize == 'small' ? '15px' : '30px'}
-			w={navSize == 'small' ? '75px' : '200px'}
+			p='5%'
 			flexDir='column'
-			justifyContent='space-between'
+			w='100%'
+			alignItems={navSize == 'small' ? 'center' : 'flex-start'}
+			as='nav'
 		>
-			<Flex
-				p='5%'
-				flexDir='column'
-				w='100%'
-				alignItems={navSize == 'small' ? 'center' : 'flex-start'}
-				as='nav'
-			>
-				<IconButton
-					background='none'
-					mt={5}
-					_hover={{ background: 'none' }}
-					icon={<FiMenu />}
-					onClick={() => {
-						if (navSize == 'small') changeNavSize('large')
-						else changeNavSize('small')
-					}}
-				/>
-				<NavItem
-					navSize={navSize}
-					icon={FiHome}
-					title='Info'
-					description='This is the description for the dashboard.'
-					active
-				/>
-				{/* <Link href='/calculator10'> */}
+			<IconButton
+				background='none'
+				mt={5}
+				_hover={{ background: 'none' }}
+				icon={<FiMenu />}
+				onClick={() => {
+					if (navSize == 'small') changeNavSize('large')
+					else changeNavSize('small')
+				}}
+			/>
+			<NavItem
+				navSize={navSize}
+				icon={FiHome}
+				title='Info'
+				description='This is the description for the dashboard.'
+				active
+			/>
+			<Link href='/calculator10'>
 				<NavItem
 					navSize={navSize}
 					icon={FiBriefcase}
 					title='Decimal calculator'
 				/>
-				{/* </Link> */}
-				<NavItem
-					navSize={navSize}
-					icon={FiSettings}
-					title='Binary calculator'
-				/>
-			</Flex>
+			</Link>
+
+			<NavItem navSize={navSize} icon={FiSettings} title='Binary calculator' />
 		</Flex>
+		// </Flex>
 	)
 }
