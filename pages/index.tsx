e67e0d051684a5239b9cc80eae9a738fd66ceaa4
Val Rakitine <vrakitine@gmail.com>
2023-10-29 20:06:11 -0400

    Updated from ted _Layout22 #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,5 @@ import { Flex, Text, IconButton } from '@chakra-ui/react'
 import { FiMenu } from 'react-icons/fi'
 
 export default function Home() {
-	return (
-		<Flex w='100%'>
-			<Flex
-				pos='absolute'
-				top='50%'
-				left='50%'
-				transform='translate(-50%, -50%)'
-			>
-Val
-			</Flex>
-		</Flex>
-	)
-}
+	return <>Index Page</>;
+  }
