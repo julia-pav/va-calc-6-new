21e354985820510ea097aa4b10a34e55a82020fc
Val Rakitine <vrakitine@gmail.com>
2023-10-31 10:17:09 -0400

    Updated export type VaScriptAction

--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,29 @@
 export type VaScriptAction =
   | "Action_init"
-  | "Action_operand_1_attach_zero";
+  | "Action_operand_1_attach_zero"
+  | "Action_operand_1_attach_one"
+  | "Action_operand_1_attach_two"
+  | "Action_operand_1_attach_three"
+  | "Action_operand_1_attach_four"
+  | "Action_operand_1_attach_five"
+  | "Action_operand_1_attach_six"
+  | "Action_operand_1_attach_seven"
+  | "Action_operand_1_attach_eight"
+  | "Action_operand_1_attach_nine"
+  | "Action_waiting_for_operand_2_for_plus"
+  | "Action_operand_2_attach_zero"
+  | "Action_operand_2_attach_one"
+  | "Action_operand_2_attach_two"
+  | "Action_operand_2_attach_three"
+  | "Action_operand_2_attach_four"
+  | "Action_operand_2_attach_five"
+  | "Action_operand_2_attach_six"
+  | "Action_operand_2_attach_seven"
+  | "Action_operand_2_attach_eight"
+  | "Action_operand_2_attach_nine"
+  | "Action_warning_10__Second_operand_is_missing"
+  | "Action_clear"
+  | "Action_show_result";
 
   export type Direction =
   | "Direction_init"
